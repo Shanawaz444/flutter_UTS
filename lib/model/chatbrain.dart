f2c17df81b68f7ae2ac9cc5a811265834b14a2ec
shanawaz444 <shanawaz0891@gmail.com>
2020-05-17 11:21:13 +0530

    created a brain for chat data

---
+++ lib/model/chatbrain.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class ChatBrain{
+  final String message;
+  final String sender;
+
+  ChatBrain({
+    @required this.message,
+    @required this.sender,
+  });
+}
